5dbe7346fb1e9d2be5a6cf599b3e826615a423d3
chmutenkoa <alenachm@gmail.com>
2023-06-03 10:14:37 +0300

    task3 join replaced by multiple aggregate

--- task3_groupby.py
+++ task3_groupby.py
@@ -6,16 +6,10 @@ more than 100 ratings. The lifetime is the difference between the maximum and
 minimum values of the timestamp column for a given userId value.
 """
 
-df = pd.read_csv('ratings.csv')
-grouped = df.groupby('userId')
+ratings = pd.read_csv('ratings.csv')
 
-min_max = grouped.agg(['min', 'max'])['timestamp']
-min_max['life_time'] = min_max['max'] - min_max['min']
-
-counts = grouped.count()
-active_users = counts.loc[counts.rating > 100]  # select users who have given > 100 ratings
-
-joined = active_users.join(min_max, how='left')
-avg_life_time = joined.life_time.mean()
+agg = ratings.groupby('userId').aggregate({'timestamp': ['min', 'max'], 'rating': 'count'})
+agg['life_time'] = agg['timestamp']['max'] - agg['timestamp']['min']
+avg_life_time = agg.loc[agg['rating']['count'] > 100]['life_time'].mean()
 
 print(avg_life_time)
